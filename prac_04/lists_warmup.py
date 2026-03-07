caca35b9a3f9ba3af1cac1853f162a6ab7fae23e
Ansuman jaiswal <ansuman.jaiswal@my.jcu.edu.au>
2026-03-08 00:50:23 +0800

    prac 3 finish import random uniform random.randint capitalist_conrad.py complete while loop file open file closed done with expectation password check file.py complete open file close file number print in txt file
    prac 4 lists warmup done

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,16 @@
+numbers =[3,1,4,1,5,9,2]
+print(numbers[0]) # 3
+print(numbers[-1]) # 2
+print(numbers[3]) # 1
+print(numbers[:-1]) #3 to 9
+print(numbers[3:4]) #1
+print(5 in numbers) # True
+print("3" in numbers) #false
+numbers + [6, 5, 3]  #
+
+numbers[0] = "ten"
+print(numbers)
+numbers[-1] = 1
+print(numbers)
+print(numbers[2:7])
+print( 9 in numbers)
