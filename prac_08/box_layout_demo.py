9032dfd5d71e943f6d3717320bad53eee7e3f398
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-16 20:31:15 +0800

    add button with name change third and first button name change

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,5 +1,18 @@
 from kivy.app import App
 from kivy.lang import Builder
+import random
+
 
 class BoxLayoutDemo(App):
     def build(self):
+        """build the kivy app from the kivy file """
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+def handle_greet(self):
+    if random.randint(1,10) <=5:
+        self.root.ids.my_label.txt = "ouch!"
+    else:
+        self.root.ids.my_label.txt = "oh no!"
+
+BoxLayoutDemo().run()
