4b39d2e96a472edcb291c2268f40afcdbf885181
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-19 10:41:13 +0800

    add greet add button

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -10,9 +10,6 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 def handle_greet(self):
-    if random.randint(1,10) <=5:
-        self.root.ids.my_label.txt = "ouch!"
-    else:
-        self.root.ids.my_label.txt = "oh no!"
-
+    print("test")
+    self.root.ids.output_label.text = "Hello "
 BoxLayoutDemo().run()
