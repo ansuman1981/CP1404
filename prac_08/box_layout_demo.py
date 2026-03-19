734ccd275f68984632f609393e16a7b1ce4cd07a
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-19 10:44:50 +0800

    upadte

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -9,7 +9,14 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
-def handle_greet(self):
-    print("test")
-    self.root.ids.output_label.text = "Hello "
-BoxLayoutDemo().run()
+
+    def handle_greet(self):
+        name = self.root.ids.input_name.text.strip()
+        if name:
+            self.root.ids.output_label.text = f"Hello {name}!"
+        else:
+            self.root.ids.output_label.text = "Please enter a name"
+
+    def handle_clear(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = 'Enter your name'
