f70463c343303302292de8c29590654235d962dc
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-26 13:50:27 +0800

    test done for drive 100

--- prac_09/texi_test.py
+++ prac_09/texi_test.py
@@ -5,5 +5,7 @@ print(my_taxi)
 my_taxi.drive(40)
 print(my_taxi)
 print(f"Current fare: ${my_taxi.get_fare():.2f}")
-my_taxi.get_fare()
-my
+my_taxi.start_fare()
+
+my_taxi.drive(100)
+print(my_taxi)
