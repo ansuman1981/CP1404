b73a91f40f28acd08ea9b3085d3aa9c2a4e621d6
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-26 14:54:30 +0800

    import random and use to create random value between 0 and 100

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,5 +1,10 @@
 from cars import Car
+import random
 class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        random.randint(0, 100)
+        if random < self.reliability:
